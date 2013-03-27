a727020a7655dd5ea8656d2bb116150db16b2de6
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-27 12:07:47 +0100

    Modified package params

--- manifests/client/package.pp
+++ manifests/client/package.pp
@@ -1,11 +1,25 @@
 class mcollective::client::package($mcollective_version, $rubygem_stomp_version) {
 
+  case $::operatingsystemrelease {
+    /^5./: {
+      $mcollective_version_real = "${mcollective_version}.el5"
+      $rubygem_stomp_version_real = "${rubygem_stomp_version}.el5"
+    }
+
+    /^6./: {
+      $mcollective_version = "${mcollective_version}.el6"
+      $rubygem_stomp_version = "${rubygem_stomp_version}..el6"
+    }
+
+    default: { notice("operatingsystemrelease ${::operatingsystemrelease} is not supported") }
+  }
+
   package { ['mcollective-common', 'mcollective-client']:
-    ensure => $mcollective_version,
+    ensure => $mcollective_version_real,
   }
 
   package { 'rubygem-stomp':
-    ensure => $rubygem_stomp_version,
+    ensure => $rubygem_stomp_version_real,
   }
 
 }
