90c1f137f6e85976b3a813a9b1344bd3ec74ec2f
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-27 09:00:59 +0100

    Implementation for client packages

--- manifests/client/package.pp
+++ manifests/client/package.pp
@@ -1,2 +1,11 @@
-class mcollective::client::package {
+class mcollective::client::package($mcollective_version, $rubygem_stomp_version) {
+
+  package { ['mcollective-common', 'mcollective-client']:
+    ensure => $mcollective_version,
+  }
+
+  package { 'rubygem-stomp':
+    ensure => $rubygem_stomp_version,
+  }
+
 }
