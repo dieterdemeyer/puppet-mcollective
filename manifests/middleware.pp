3954dfc40e7a4ec8987a4187f9e1251c9e7631d7
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-29 20:24:13 +0100

    Initial implementation for middleware

--- manifests/middleware.pp
+++ manifests/middleware.pp
@@ -1,2 +1,8 @@
 class mcollective::middleware {
+
+  include mcollective::middleware::package
+  include mcollective::middleware::service
+
+  Class['mcollective::middleware::package'] -> Class['mcollective::middleware::config'] ~> Class['mcollective::middleware::service']
+
 }
