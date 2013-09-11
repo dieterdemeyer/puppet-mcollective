b700dca39b80c16fe87c653c50a1460498bb98c3
Jurgen De Backer <jurgen.debacker@cegeka.be>
2013-09-11 16:08:16 +0200

    Support mcollective message ssl verification (message_ssl => true)

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -1,4 +1,4 @@
-class mcollective::client::config($broker_pool_config=[]) {
+class mcollective::client::config($broker_pool_config=[], $message_ssl = false) {
 
   file { '/etc/mcollective/client.cfg' :
     ensure  => file,
@@ -8,4 +8,46 @@ class mcollective::client::config($broker_pool_config=[]) {
     content => template("${module_name}/client/client.cfg.erb")
   }
 
+  if ($message_ssl) {
+
+    file {'/etc/mcollective/ssl/private_keys' :
+      ensure => directory,
+      owner  => root,
+      group  => root,
+      mode   => '0755',
+    }
+
+    file {'/etc/mcollective/ssl/public_keys' :
+      ensure => directory,
+      owner  => root,
+      group  => root,
+      mode   => '0755',
+    }
+
+    file {'/etc/mcollective/ssl/private_keys/mcoclient-priv.pem' :
+      ensure => file,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => "puppet:///modules/${module_name}/ssl/mcoclient-priv.pem",
+    }
+    
+    file {'/etc/mcollective/ssl/public_keys/mcoclient-pub.pem' :
+      ensure => file,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => "puppet:///modules/${module_name}/ssl/mcoclient-pub.pem",
+    }
+
+    # Agent's public key:
+    file {'/etc/mcollective/ssl/public_keys/mcoserver-pub.pem' :
+      ensure => file,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => "puppet:///modules/${module_name}/ssl/mcoserver-pub.pem",
+    }
+
+  }
 }
