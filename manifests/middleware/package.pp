686ae68f8caee6003fe6092003e43904bb1eaefc
Jurgen De Backer <jurgen.debacker@cegeka.be>
2014-01-27 10:16:19 +0100

    Check Red Hat release for package version of ActiveMQ

--- manifests/middleware/package.pp
+++ manifests/middleware/package.pp
@@ -2,8 +2,14 @@ class mcollective::middleware::package {
 
   include mcollective::common::package
 
+  if ($operatingsystemmajrelease == '6') {
+    $osrelease = 'el6'
+  } else {
+    $osrelease = 'el5'
+  }
+
   package { ['activemq', 'activemq-info-provider']:
-    ensure => '5.5.0-1.el6',
+    ensure => "5.5.0-1.${osrelease}",
   }
 
   package { ['tanukiwrapper', 'rubygems']:
