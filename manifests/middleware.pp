5ab0401dcd8b5d991a3e05863a3dd4bc8c9d1e9d
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-01-28 10:06:50 +0100

    Fix activemq packages by using other fact + introduce version parameter

--- manifests/middleware.pp
+++ manifests/middleware.pp
@@ -1,4 +1,4 @@
-class mcollective::middleware($type=undef, $ssl=false) {
+class mcollective::middleware($type=undef, $ssl=false, $activemq_version='5.5.0-1') {
 
   if $type in ['noc', 'customer'] {
     $type_real = $type
@@ -6,7 +6,10 @@ class mcollective::middleware($type=undef, $ssl=false) {
     fail('Mcollective::Middleware: parameter type must be noc or customer')
   }
 
-  include mcollective::middleware::package
+  class { 'mcollective::middleware::package':
+    activemq_version => $activemq_version
+  }
+
   include mcollective::middleware::service
 
   class { 'mcollective::middleware::config':
--- manifests/middleware/package.pp
+++ manifests/middleware/package.pp
@@ -1,15 +1,15 @@
-class mcollective::middleware::package {
+class mcollective::middleware::package($activemq_version='5.5.0-1') {
 
   include mcollective::common::package
 
-  if ($operatingsystemmajrelease == '6') {
-    $osrelease = 'el6'
-  } else {
-    $osrelease = 'el5'
+  case $::operatingsystemrelease {
+    /^5./: { $osrelease = 'el5' }
+    /^6./: { $osrelease = 'el6' }
+    default: { notice("operatingsystemrelease ${::operatingsystemrelease} is not supported") }
   }
 
   package { ['activemq', 'activemq-info-provider']:
-    ensure => "5.5.0-1.${osrelease}",
+    ensure => "${activemq_version}.${osrelease}",
   }
 
   package { ['tanukiwrapper', 'rubygems']:
